66cc844c293460400aadead205b4c1ff324a4e6a
venkatduddukuri <vduddukuri@my365.bellevue.edu>
2023-04-08 07:41:00 +0530

    week 4 assignment

---
+++ src/main/java/com/bookclub/service/dao/WishlistDao.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package com.bookclub.service.dao;
+
+import com.bookclub.model.WishlistItem;
+import com.bookclub.service.GenericDao;
+
+/**
+ * @author venkatduddukuri
+ *
+ */
+public interface WishlistDao extends GenericDao<WishlistItem, String> {
+
+}
